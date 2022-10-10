f3538c558c0bce9c830b4e161036331575676ee6
amjayu <115442118+amjayu@users.noreply.github.com>
2022-10-10 13:00:33 +0530

    fn

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 echo "Hello There To Check My Website Click The Link Below:\n";
 echo "<br>";
 echo "<br>";
-echo "<a  href='home1.html'>Hey You Are You Looking For Me ?</a>";
+echo "<a  href='home1.html' text-decoration:none>Hey You Are You Looking For Me ?</a>";
 
 ?>
