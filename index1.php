4aca2bd88574f6ad7b049ad05c22962848956bf3
amjayu <115442118+amjayu@users.noreply.github.com>
2022-10-11 10:53:37 +0530

    finall

--- index1.php
+++ index1.php
@@ -4,5 +4,4 @@ echo "Hello There To Check My Website Click The Link Below:\n";
 echo "<br>";
 echo "<br>";
 echo "<a  href='home1.html' text-decoration:none>Hey You Are You Looking For Me ?</a>";
-
 ?>
